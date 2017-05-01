619a653122048e1189900f189e5bf57144bfa71b
narayana <narayana@narayana-PC>
2017-05-02 00:23:42 +0530

    spring aop programatic project structure

---
+++ SpringAopProgramatic/src/main/java/com/spring/programatic/SpringAopProgramatic/App.java
@@ -0,0 +1,13 @@
+package com.spring.programatic.SpringAopProgramatic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello fhhh World!" );
+    }
+}
