46d7d7b617bdbaf568953ba43ecfa1062d30dbec
narayana <narayana@narayana-PC>
2017-05-01 23:57:51 +0530

    added pointcut expresiions

--- springaop/src/main/java/com/test/TestAop.java
+++ springaop/src/main/java/com/test/TestAop.java
@@ -6,10 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.springaop.customer.bo.CustomerBo;
 
 public class TestAop {
-	public static void main(String[] args) {
+	public static void main(String[] args)throws Exception {
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
 		CustomerBo customer = (CustomerBo) applicationContext.getBean("customerBo");
 		customer.addCustomerAround("Welcome");
+		customer.addCustomer();
+		customer.addCustomerThrowException();
 		System.out.println("Done");
 	}
 
